24744c967b5b44a40c3b1a67ff28490b445a7155
傅海平 <haipingf@gmail.com>
2011-10-13 21:32:19 +0800

    Edited Code/class-2/syscall-1.s via GitHub

--- Code/class-2/syscall-1.s
+++ Code/class-2/syscall-1.s
@@ -1,7 +1,7 @@
 # syscall-1.s - An example of passing input values to a system call
 .section .data
 output:
-	.ascii “This is a test message.\n”
+	.ascii "This is a test message.\n"
 output_end:
 	.equ len, output_end - output
 .section .text
